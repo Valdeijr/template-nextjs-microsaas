18cdc513a3808a0ccd49afdf9ac3453367b33c76
Valdeijr <valdei.ferreira.jr@gmail.com>
2025-04-18 11:40:40 -0300

    FirestoreInclude

--- src/infrastructure/auth/providers/authProviders.ts
+++ src/infrastructure/auth/providers/authProviders.ts
@@ -1,7 +1,12 @@
+import { firebaseCert } from '@/infrastructure/firebase/firebase'
+import { FirestoreAdapter } from '@auth/firebase-adapter'
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google, GitHub],
+  adapter: FirestoreAdapter({
+    credential: firebaseCert,
+  }),
 })
